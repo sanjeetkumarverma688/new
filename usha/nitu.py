af63d9a620e18d918913c881858e3330111a59d1
sanjeetkumarverma688 <sanjeetverma112008@gmail.com>
2024-09-18 20:51:28 +0530

    Add files via upload

---
+++ usha/nitu.py
@@ -0,0 +1,37 @@
+apna = "sanjet"
+print(apna)
+sanjet="rohit"
+print(sanjet)
+age=45
+print(age)
+price=4.8
+print(price)
+print(type(apna))
+print(type(age))
+print(type(price))
+name1="bk"
+name2='sk'
+name3='''rk'''
+print(name1)
+print(name2)
+print(name3)
+# print summ  
+a=5 
+b=11
+sum=a+b
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+
+#relation operater
+
+s=9
+d=10
+print(s==d)
+print(s>=d)
+print(s<=d)
+print(s<d)
+print(s>d)
